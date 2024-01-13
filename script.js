e0e63b297b55713d8706d1a07312929c971bf5bb
Jeremy Riechert <j.riechert@secova.de>
2024-01-13 22:31:28 +0100

    improve  api request

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-fetch('https://api.scryfall.com/cards/56ebc372-aabd-4174-a943-c7bf59e5028d', {
-    method: 'GET'
-}).then(res => {
-    return res.json()
-}).then(card => fillInDetails(card))
+function getRandomCard() {
+    return new Promise (function (resolve, reject) {
+        fetch('https://api.scryfall.com/cards/random', {
+            method: 'GET'
+        }).then(res => {
+            resolve(res.json())
+        })
+    })
+};
 
-function fillInDetails(card) {
-    document.getElementsByClassName('card-description')[0].innerHTML = card.oracle_text;
-} 
+async function fillInDetails() {
+    let randomCard = await getRandomCard();
+    console.log(randomCard)
+    document.getElementsByClassName('card-image')[0].setAttribute('src', randomCard.image_uris.normal);
+    document.getElementsByClassName('card-description')[0].innerHTML = randomCard.oracle_text;
+}
+
+fillInDetails();
