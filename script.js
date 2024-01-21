e8efe017bfe26312c81f61a24c071fea499fcea3
Jeremy Riechert <j.riechert@secova.de>
2024-01-21 21:51:57 +0100

    insert mana icons

--- script.js
+++ script.js
@@ -11,8 +11,14 @@ function getRandomCard() {
 async function fillInDetails() {
     let randomCard = await getRandomCard();
     console.log(randomCard)
+    document.getElementsByClassName('card-title')[0].innerHTML = randomCard.name;
+    document.getElementsByClassName('card-mana-cost')[0].innerHTML = 'Mana cost: ' + insertIcons(randomCard.mana_cost);
     document.getElementsByClassName('card-image')[0].setAttribute('src', randomCard.image_uris.normal);
     document.getElementsByClassName('card-description')[0].innerHTML = randomCard.oracle_text;
 }
+
+function insertIcons(text) {
+    return text.replace(/{[\d]}/, '<img src="resources/mana_one.svg" class="mana-cost-icon"></img>');
+};
 
 fillInDetails();
