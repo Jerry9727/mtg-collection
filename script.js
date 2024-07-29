f8fa94f6d9c90702e4860556636db637f6815cc2
Jeremy Riechert <j.riechert@secova.de>
2024-07-29 13:21:13 +0200

    reworks, old mac

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-const manaMaster = () => {
-    fetch("/resources/mana-master.mana-master.json")
-}
-console.log(manaMaster)
-
 function getRandomCard() {
     return new Promise(function (resolve, reject) {
         fetch('https://api.scryfall.com/cards/random', {
@@ -13,18 +8,31 @@ function getRandomCard() {
     })
 };
 
-async function fillInDetails() {
+//Von Element abhängig machen
+async function fillInDetails(id) {
+    element = document.getElementById('card-container-' + id);
     let randomCard = await getRandomCard();
     console.log(randomCard)
     document.getElementsByClassName('card-title')[0].innerHTML = randomCard.name;
-    document.getElementsByClassName('card-mana-cost')[0].innerHTML = 'Mana cost: ' + insertIcons(randomCard.mana_cost);
+    document.getElementsByClassName('card-mana-cost')[0].innerHTML = 'Mana cost: ' + insertCardSymbols(randomCard.mana_cost);
     document.getElementsByClassName('card-image')[0].setAttribute('src', randomCard.image_uris.normal);
-    document.getElementsByClassName('card-description')[0].innerHTML = randomCard.oracle_text;
+    document.getElementsByClassName('card-description')[0].innerHTML = insertCardSymbols(randomCard.oracle_text);
 }
 
-function insertIcons(text) {
-    let obj = JSON.parse()
-    return text.replace(/{[\d]}/, '<img src="resources/mana_one.svg" class="mana-cost-icon"></img>');
+//tap icon doesn't work. Scryfall returns "{t}" but mana-master expects "tap"
+function insertCardSymbols(text) {
+    let returnText = text;
+    returnText = returnText.replace(/{(.*?)}/g, function(match, p1) {
+        return '{' + p1.toLowerCase() + '}';
+    });
+
+    returnText = returnText.replaceAll('{', '<i class="ms ms-')
+        .replaceAll('}', ' ms-cost ms-shadow"></i>');
+
+    return returnText;
 };
 
 fillInDetails();
+
+
+//Write function to insert as many cards as wanted
